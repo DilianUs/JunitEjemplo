9ed815b22665d124940be74a49e043f11a610023
DilianUs <75262812+DilianUs@users.noreply.github.com>
2022-10-11 12:14:17 -0500

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,28 @@
+package com.mycompany.mavenproject3;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author braul
+ */
+public class Calculadora {
+    public int sumar(int numUno,int numDos){
+        return numUno + numDos;
+    }
+    
+    public int restar(int numUno,int numDos){
+        return numUno - numDos;
+    }
+      
+    public int multiplicar(int numUno,int numDos){
+        return numUno * numDos;
+    }
+        
+    public int dividir(int numUno,int numDos){
+        return numUno / numDos;
+    }
+}
